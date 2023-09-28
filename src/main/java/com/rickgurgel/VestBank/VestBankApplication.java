9f3a90a5a0e4c03003781b6d82bc959761c1d6ec
rickgurgel <rgurgelf@gmail.com>
2023-09-27 22:56:38 -0300

    first commit

---
+++ src/main/java/com/rickgurgel/VestBank/VestBankApplication.java
@@ -0,0 +1,13 @@
+package com.rickgurgel.VestBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VestBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rickgurgel/VestBank/VestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickgurgel.VestBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
