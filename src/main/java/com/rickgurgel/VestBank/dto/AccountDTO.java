b73257a1678b06b4bdd6d51da26908d8f97d439f
rickgurgel <rickgurgel>
2023-10-01 01:22:45 -0300

    usando padrão DTO para retornar usuários

---
+++ src/main/java/com/rickgurgel/VestBank/dto/AccountDTO.java
@@ -0,0 +1,49 @@
+package com.rickgurgel.VestBank.dto;
+
+import java.io.Serializable;
+
+import com.rickgurgel.VestBank.domain.Account;
+
+public class AccountDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String username;
+	private Double balance;
+	
+	public AccountDTO() {
+		
+	}
+	
+	public AccountDTO(Account obj) {
+		id = obj.getId();
+		username = obj.getUsername();
+		balance = obj.getBalance();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+	
+	
+}
--- src/main/java/com/rickgurgel/VestBank/resources/AccountResource.java
+++ src/main/java/com/rickgurgel/VestBank/resources/AccountResource.java
@@ -1,6 +1,7 @@
 package com.rickgurgel.VestBank.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rickgurgel.VestBank.domain.Account;
+import com.rickgurgel.VestBank.dto.AccountDTO;
 import com.rickgurgel.VestBank.services.AccountService;
 
 @RestController
@@ -19,8 +21,9 @@ public class AccountResource {
 	private AccountService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Account>> findAll(){
+	public ResponseEntity<List<AccountDTO>> findAll(){
 		List<Account> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<AccountDTO> listDto = list.stream().map(x -> new AccountDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
