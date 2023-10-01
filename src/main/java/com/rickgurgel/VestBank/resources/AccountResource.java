63ab6b76bb98cf49f495ca05175bc6f46f00e72e
rickgurgel <rickgurgel>
2023-10-01 02:05:18 -0300

    obtendo usuario por id + tratamento de exceções

--- src/main/java/com/rickgurgel/VestBank/resources/AccountResource.java
+++ src/main/java/com/rickgurgel/VestBank/resources/AccountResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +27,11 @@ public class AccountResource {
 		List<AccountDTO> listDto = list.stream().map(x -> new AccountDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<AccountDTO> findById(@PathVariable String id){
+		Account obj = service.findById(id);
+		return ResponseEntity.ok().body(new AccountDTO(obj));
+	}
+	
 }
--- src/main/java/com/rickgurgel/VestBank/services/AccountService.java
+++ src/main/java/com/rickgurgel/VestBank/services/AccountService.java
@@ -1,12 +1,14 @@
 package com.rickgurgel.VestBank.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rickgurgel.VestBank.domain.Account;
 import com.rickgurgel.VestBank.repository.AccountRepository;
+import com.rickgurgel.VestBank.services.exception.ObjectNotFoundException;
 
 @Service
 public class AccountService {
@@ -16,5 +18,10 @@ public class AccountService {
 	
 	public List<Account> findAll(){
 		return repo.findAll();
+	}
+	
+	public Account findById(String id){
+		Optional<Account> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
