2dece2039749bbfbc44c5c18e4430a3f0dbb4c2f
rickgurgel <rickgurgel>
2023-10-01 01:09:31 -0300

    operação de instanciação da base de dados

---
+++ src/main/java/com/rickgurgel/VestBank/config/Instantiation.java
@@ -0,0 +1,30 @@
+package com.rickgurgel.VestBank.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.rickgurgel.VestBank.domain.Account;
+import com.rickgurgel.VestBank.repository.AccountRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		accountRepository.deleteAll();
+			
+		Account ricardo = new Account(null, "Ricardo Gurgel", 1234.56);
+		Account arielton = new Account(null, "Arielton Nunes", 23456.78);
+		Account guanabara = new Account(null, "Guanabara Nunes", 6666.66);
+		
+		accountRepository.saveAll(Arrays.asList(ricardo, arielton, guanabara));
+	}
+
+}
